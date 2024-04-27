47f44561a74fce11918517b343d5ed0cfc3c3771
Master_Joel <86199291+Rodrigo30000@users.noreply.github.com>
2024-04-27 18:56:28 -0400

    subiendo el archivo python

---
+++ ejemplo.py
@@ -0,0 +1,10 @@
+print("primer commit")
+#aqui empieza mi parte 
+print("segundo commit añadido")
+print(2+2)
+#aqui termina el segundo commit
+
+#aqui empieza la tercera parte
+print("tercer commit añadido")
+print(3+3)
+#aqui termina el tercer commit
