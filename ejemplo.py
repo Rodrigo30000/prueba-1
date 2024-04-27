4d07522fc79f7b726afd8794a8efa917305bb2d0
Joel <rodrigojoelaguirre@gmail.com>
2024-04-27 19:22:58 -0400

    cambiando todo

--- ejemplo.py
+++ ejemplo.py
@@ -1,10 +1,1 @@
-print("primer commit")
-#aqui empieza mi parte 
-print("segundo commit añadido")
-print(2+2)
-#aqui termina el segundo commit
-
-#aqui empieza la tercera parte
-print("tercer commit añadido")
-print(3+3)
-#aqui termina el tercer commit
+print("prueba remota")
